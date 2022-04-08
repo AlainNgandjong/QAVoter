0a1768abc3aae7d4fbd9a1d13ca0c4c21a0695d8
AlainNgandjong <a.ngandjong@gmail.com>
2022-04-08 20:47:27 +0200

    create a command and log in the log file + sentry config

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
