7fb94b31faaa7525b7be53aca0131b68a1aca1cb
AlainNgandjong <a.ngandjong@gmail.com>
2022-04-14 14:45:59 +0200

    Add Question entity and new action in question controller

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Question;
+use Doctrine\ORM\EntityManagerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,36 @@ class QuestionController extends AbstractController
     public function index(): Response
     {
         return $this->render('question/homepage.html.twig');
+    }
+
+    #[Route('/questions/new', name: 'app_question_new')]
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
+        }
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
     }
 
     #[Route('/questions/{slug}', name: 'app_question_show')]
@@ -32,4 +64,6 @@ class QuestionController extends AbstractController
             'questionText' => $questionText
         ]);
     }
+
+
 }
