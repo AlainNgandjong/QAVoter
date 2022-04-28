bdda67e523cabdd81bada13dbc20fa98549c8708
AlainNgandjong <a.ngandjong@gmail.com>
2022-04-29 00:04:05 +0200

    install orm-fixtures and create QuestionFixtures

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
